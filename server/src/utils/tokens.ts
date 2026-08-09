0c883ac81e874a026bd453e81cba8a22472380ba
GamalHafez <gamal.k.hafez@gmail.com>
2026-08-09 21:00:52 +0300

    feat(auth): add refresh token and access token generation utilities

--- server/src/utils/tokens.ts
+++ server/src/utils/tokens.ts
@@ -1,8 +1,13 @@
 import jwt, { type SignOptions } from 'jsonwebtoken';
 import type { Response } from 'express';
+import type { User } from '@shared/types/index.js';
 
-export const generateToken = (userId: string, res: Response) => {
-  const payload = { id: userId };
+export const generateAccessToken = (user: User, res: Response) => {
+  const {
+    id,
+    role: { name: role },
+  } = user;
+  const payload = { sub: id, role };
 
   const secret = process.env.JWT_SECRET;
   if (!secret) {
@@ -10,16 +15,35 @@ export const generateToken = (userId: string, res: Response) => {
   }
 
   const expiresIn =
-    (process.env.JWT_EXPIRES_IN as SignOptions['expiresIn']) || '7d';
+    (process.env.ACCESS_TOKEN_EXPIRES_IN as SignOptions['expiresIn']) || '15m';
 
-  const token = jwt.sign(payload, secret, { expiresIn });
+  const accessToken = jwt.sign(payload, secret, { expiresIn });
 
-  res.cookie('jwt', token, {
+  res.cookie('accessToken', accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
-    maxAge: 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 15,
   });
+};
 
-  return token;
+export const generateRefreshToken = (user: User, res: Response) => {
+  const payload = { sub: user.id };
+
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error('JWT_SECRET is not defined in environment variables');
+  }
+
+  const expiresIn =
+    (process.env.REFRESH_TOKEN_EXPIRES_IN as SignOptions['expiresIn']) || '30d';
+
+  const refreshToken = jwt.sign(payload, secret, { expiresIn });
+
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
 };

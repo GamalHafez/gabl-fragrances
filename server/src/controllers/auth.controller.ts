0f9afa35e760bbbac8316d9cf73dc39328aab836
GamalHafez <gamal.k.hafez@gmail.com>
2026-08-10 17:53:55 +0300

    refactor(auth): improve auth service and cookie handling

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,86 +1,17 @@
 import type { Request, Response, NextFunction } from 'express';
-import { prisma } from '@/config/db.js';
-import bcrypt from 'bcrypt';
-import { sendError, sendSuccess } from '@/utils/response.js';
-import {
-  generateAccessToken,
-  generateRefreshToken,
-  getTokenExpiresAt,
-  hashToken,
-  setCookie,
-} from '@/utils/tokens.js';
-import { authConfig } from '@/config/auth.js';
+import { sendSuccess } from '@/utils/response.js';
+import { setCookie } from '@/utils/tokens.js';
 import { authService } from '@/services/auth.service.js';
-
-const SALT_ROUNDS = 12;
 
 export const signUp = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const { name, email, password } = req.body;
-
   try {
-    // Check existing user
-    const existingUser = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (existingUser) {
-      return sendError(res, {
-        statusCode: 409,
-        message: 'User already exists with this email',
-      });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-
-    // Create customer
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-        role: {
-          connect: {
-            name: 'CUSTOMER',
-          },
-        },
-      },
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        role: {
-          select: {
-            id: true,
-            name: true,
-          },
-        },
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
-
-    // Generate access token
-    const accessToken = generateAccessToken(user, res);
-
-    // Generate refresh token
-    const refreshToken = generateRefreshToken(user, res);
-
-    // Hash refresh token
-    const tokenHash = hashToken(refreshToken);
-
-    // Store hash in DB
-    await prisma.refreshToken.create({
-      data: {
-        tokenHash,
-        userId: user.id,
-        expiresAt: getTokenExpiresAt(authConfig.refreshTokenExpiresIn),
-      },
-    });
+    const { user, accessToken, refreshToken } = await authService.signUp(
+      req.body,
+    );
 
     // set the cookies
     setCookie(res, 'accessToken', accessToken, 15 * 60 * 1000);
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { loginSchema } from '@shared/schemas/auth.validators.js';
+import { loginSchema, signupSchema } from '@shared/schemas/auth.validators.js';
 import z from 'zod';
 import { prisma } from '@/config/db.js';
 import { AppError } from '@/utils/response.js';
@@ -13,8 +13,71 @@ import { User } from '@shared/types/user.js';
 import { authConfig } from '@/config/auth.js';
 
 type LoginInput = z.infer<typeof loginSchema>;
+type SignUpInput = z.infer<typeof signupSchema>;
+const SALT_ROUNDS = 12;
 
 export const authService = {
+  async signUp(data: SignUpInput) {
+    const { name, email, password } = data;
+
+    const existingUser = await this.findUserByEmail(email);
+    if (existingUser) {
+      throw new AppError(409, 'User already exists with this email');
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const user = await this.createUser(name, email, hashedPassword);
+
+    const { accessToken, refreshToken } = this.generateTokens(user);
+    const tokenHash = hashToken(refreshToken);
+    await this.createRefreshToken(tokenHash, user.id);
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
+  },
+
+  async findUserByEmail(userEmail: string) {
+    return await prisma.user.findUnique({
+      where: { email: userEmail },
+      select: {
+        id: true,
+      },
+    });
+  },
+
+  async createUser(name: string, email: string, hashedPassword: string) {
+    return await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: {
+          connect: {
+            name: 'CUSTOMER',
+          },
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  },
+
   async login(data: LoginInput) {
     const { email, password: userPassword } = data;
 
@@ -30,7 +93,7 @@ export const authService = {
 
     const { accessToken, refreshToken } = this.generateTokens(safeUser);
     const tokenHash = hashToken(refreshToken);
-    await this.storeHashInDb(tokenHash, safeUser.id);
+    await this.createRefreshToken(tokenHash, safeUser.id);
 
     return {
       user: safeUser,
@@ -75,7 +138,7 @@ export const authService = {
     };
   },
 
-  async storeHashInDb(tokenHash: string, userId: string) {
+  async createRefreshToken(tokenHash: string, userId: string) {
     await prisma.refreshToken.create({
       data: {
         tokenHash,
